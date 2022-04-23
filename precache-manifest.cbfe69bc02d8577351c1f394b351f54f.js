96aed320145c13c5773c2d7a15dc90ef417b2aa5
Gabriel <76435193+gncorreia@users.noreply.github.com>
2022-04-23 13:18:29 -0300

    Updates

---
+++ precache-manifest.cbfe69bc02d8577351c1f394b351f54f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ac672aee41f02c2a4b4a8043984b5b",
+    "url": "/movie-cards-library/index.html"
+  },
+  {
+    "revision": "455d18aeb861ebbe9c6b",
+    "url": "/movie-cards-library/static/css/main.35f0ea2c.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "455d18aeb861ebbe9c6b",
+    "url": "/movie-cards-library/static/js/main.606afa00.chunk.js"
+  },
+  {
+    "revision": "a95db5ba2439b13bdc08",
+    "url": "/movie-cards-library/static/js/runtime-main.a37d5318.js"
+  }
+]);
